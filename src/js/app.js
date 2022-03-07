1c71af41e02a6a1bd4d8c59e6e824d64123e55b9
forevor304 <fforest79@gmail.com>
2022-03-08 01:55:10 +0300

    redesign main paige and create second page

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,11 @@ flsFunctions.isWebp();
 import Swiper, { Navigation, Pagination } from 'swiper';
 const swiper = new Swiper();
 */
+
+window.addEventListener('scroll',function(){
+    var nav = this.document.querySelector("#nav");
+    nav.classList.toggle('navigation_sticky', window.scrollY > 0);
+    var nav_logo = this.document.querySelector("#nav_logo");
+    nav_logo.classList.toggle('navigation__logo_sticky', window.scrollY > 0);
+
+})
