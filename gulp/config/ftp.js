7ddb823358423bf3fa2f77b5a8477bd7af4aca9c
forevor304 <fforest79@gmail.com>
2022-05-17 02:33:18 +0300

    add gallery and log in popup and swiper

--- gulp/config/ftp.js
+++ gulp/config/ftp.js
@@ -1,6 +1,6 @@
 export let configFTP = {
-	host: "", // Адрес FTP сервера
-	user: "", // Имя пользователя
-	password: "", // Пароль
+	host: "37.140.192.191", // Адрес FTP сервера
+	user: "u1618780", // Имя пользователя
+	password: "XM2Tr9NfAbI4l5Kh", // Пароль
 	parallel: 5 // Кол-во одновременных потоков
 }
--- src/js/app.js
+++ src/js/app.js
@@ -14,3 +14,78 @@ window.addEventListener('scroll',function(){
     nav_logo.classList.toggle('navigation__logo_sticky', window.scrollY > 0);
 
 })
+
+let swiper = new Swiper(".swiper", {
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
+
+  document.querySelector("#show-login").addEventListener("click", function(){
+    document.querySelector('.popup').classList.add("popup__active");
+  });
+  document.querySelector(".popup__close-btn").addEventListener("click", function(){
+    document.querySelector('.popup').classList.remove("popup__active");
+  });
+
+
+  let galleryImages = document.querySelectorAll(".gallery-img");
+  let getLatestOpendImg;
+  let windowWidth = window.innerWidth;
+
+if(galleryImages) {
+    galleryImages.forEach(function(image , index) {
+		image.onclick = function() {
+			let getElementCss = window.getComputedStyle(image);	
+			let getFullImgUrl = getElementCss.getPropertyValue("background-image");
+			let getImgUrlPos = getFullImgUrl.split("/img/");
+			let setNewImgUrl = getImgUrlPos[1].replace('")','');
+
+			getLatestOpendImg = index + 1;
+
+			let container = document.body;
+			let newImgWindow = document.createElement("div");
+			container.appendChild(newImgWindow);
+			newImgWindow.setAttribute("class", "img-window");
+			newImgWindow.setAttribute("onclick", "closeImg()");
+
+			let newImg = document.createElement('img');
+			newImgWindow.appendChild(newImg);
+			newImg.setAttribute('src','img/' + setNewImgUrl);
+
+			newImg.onload = function() {
+				let imgWidth = this.width;
+				let calcImgToEdge = ((windowWidth - imgWidth) / 2) - 80;
+
+				let newNextBtn = document.createElement('a');
+				let btnNextText = document.createTextNode('>');
+				newNextBtn.appendChild(btnNextText);
+				container.appendChild(newNextBtn);
+				newNextBtn.setAttribute('class','img-btn-next');
+				newNextBtn.setAttribute('onclick','changeImg()');
+				newNextBtn.style.cssText = "right:" + calcImgToEdge + " px;";
+
+				let newPrevBtn = document.createElement('a');
+				let btnPrevText = document.createTextNode('<');
+				newPrevBtn.appendChild(btnPrevText);
+				container.appendChild(newPrevBtn);
+				newPrevBtn.setAttribute('class','img-btn-prev');
+				newPrevBtn.setAttribute('onclick','changeImg()');
+				newPrevBtn.style.cssText = "left:" + calcImgToEdge + " px;";
+
+			}	
+		}
+    });
+}
+
+
+function closeImg() {
+	document.querySelector(".img-window").remove();
+}
